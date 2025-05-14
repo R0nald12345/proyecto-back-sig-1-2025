36b79cdb4597035b311906a00a5d5b3ad39ae03f
runinbk <runinbk@gmail.com>
2025-05-14 08:48:29 -0400

    feat(route): Implement CreateRouteDto with validation decorators

--- src/modules/route/dto/create-route.dto.ts
+++ src/modules/route/dto/create-route.dto.ts
@@ -1,1 +1,36 @@
-export class CreateRouteDto {}
+import { IsString, IsNumber, IsDate, IsOptional, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateRouteDto {
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  date: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  hour_end: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Type(() => Number)
+  delivery_quantity: number;
+
+  @IsString()
+  @IsNotEmpty()
+  hour_start: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Type(() => Number)
+  total_distance: number;
+
+  @IsOptional()
+  @IsString()
+  polyline?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  deliveryId?: number;
+}
