fba48daff14b1a2a3ab495df0873b679a678dce6
runinbk <runinbk@gmail.com>
2025-05-14 06:27:10 -0400

    fead(client): Added CreateClientDto class with validations for client fields

--- src/modules/client/dto/create-client.dto.ts
+++ src/modules/client/dto/create-client.dto.ts
@@ -1,1 +1,26 @@
-export class CreateClientDto {}
+import { IsString, IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateClientDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsOptional()
+  dateRegister?: Date;
+}
